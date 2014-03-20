fc7be6ae564d0a118083507b30a86939b8b2d2d5
SamGiles <aphro.cag@googlemail.com>
2014-03-20 14:20:04 +0000

    Added fetch and add (FAA) atomic primitive

--- atomic.h
+++ atomic.h
@@ -1,12 +1,20 @@
-// If available use __sync_bool_compare_and_swap
-#if (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) > 40100
-	#define cas __sync_bool_compare_and_swap
+#ifndef _ATOMIC_H
+#define _ATOMIC_H
+
+#if defined(__llvm__) || (defined(__GNUC__) && ( __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1)))
+#    define HAS_SYNC_BOOL_COMPARE_AND_SWAP
+#    define HAS_SYNC_FETCH_AND_ADD
+#endif
+
+
+#ifdef HAS_SYNC_BOOL_COMPARE_AND_SWAP
+#  define compare_and_swap __sync_bool_compare_and_swap
 
 // If __sync_bool_compare_and_swap is not available but architecture is x86,
 // use inline asm with cmpxchgq or cmpxchgl
 #elif defined(__amd64__) || defined(__i386__)
 
-static inline int cas(unsigned long *thevalue, unsigned long oldvalue, unsigned long newvalue) {
+static inline int compare_and_swap(unsigned long *thevalue, unsigned long oldvalue, unsigned long newvalue) {
 	unsigned long prev;
 	asm volatile("lock;"
 #if defined(__amd64__)
@@ -21,4 +29,26 @@ static inline int cas(unsigned long *thevalue, unsigned long oldvalue, unsigned 
 }
 #else
 # error "define cmpxchg (compare and swap) for your architecture - (cas)"
+#endif
+#endif
+
+#ifdef HAS_SYNC_FETCH_AND_ADD
+#	define fetch_and_add __sync_fetch_and_add
+#elif defined(__amd64__) || defined(__i386)
+static inline unsigned long fetch_and_add(unsigned long* ptr, unsigned long value) {
+	unsigned long previous;
+	// xadd r0, r1   - Exchange r0 and r1 loading sum into r1
+	asm volatile ("lock;"
+#if defined(__amd64__)
+			"xaddq %1, %2;"
+#else
+			"xaddl %1, %2;"
+#endif
+			: "=r"(previous)
+			: "0"(value), "m"(*ptr)
+			: "memory");
+	return previous;
+}
+#else
+# error "define fetch and add for your architecture - (faa)"
 #endif
--- main.c
+++ main.c
@@ -11,7 +11,7 @@ void test_compare_and_swap_long() {
 	unsigned long oldvalue = *atomic_counter;
 	unsigned long newvalue = oldvalue + 1;
 
-	int cas_result = cas(atomic_counter, oldvalue, newvalue);
+	int cas_result = compare_and_swap(atomic_counter, oldvalue, newvalue);
 
 	printf("CAS (int): %d\n", cas_result);
 	printf("atomic_counter: %lu\n", *atomic_counter);
@@ -19,8 +19,21 @@ void test_compare_and_swap_long() {
 	free(atomic_counter);
 }
 
+void test_fetch_and_add() {
+	unsigned long * atomic_counter = (unsigned long*)malloc(sizeof(unsigned long));
+	*atomic_counter = 0;
+
+	unsigned long faa_result = fetch_and_add(atomic_counter, 13);
+
+	printf("FAA (int): %lu\n", faa_result);
+	printf("atomic_counter: %lu\n", *atomic_counter);
+
+	free(atomic_counter);
+}
+
 int main(int argc, char** argv) {
 	test_compare_and_swap_long();
+	test_fetch_and_add();
 	return 0;
 }
 
